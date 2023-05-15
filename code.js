2da2ff108f9216bb7b06278e6603a52149322bd3
Chinaza Ezinne <ezinne.chinaza@gmail.com>
2023-05-15 13:41:55 -0400

    added requires

--- code.js
+++ code.js
@@ -1,1 +1,9 @@
-// Test
+const http = require("http");
+const express = require("express")
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') });
+
+
+const app = express();
