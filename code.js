250d33d6f58a4a25fe4656d229667139a3cc3f79
Chinaza Ezinne <ezinne.chinaza@gmail.com>
2023-05-16 19:14:08 -0400

    untest

--- code.js
+++ code.js
@@ -25,7 +25,6 @@ serverStartDetails();
     correct port, stop command processing, page folder setting, 
     and page rendering(through app listen)
 */
-/* test */
 function serverStartDetails(){
     process.stdin.setEncoding("utf8");
 
